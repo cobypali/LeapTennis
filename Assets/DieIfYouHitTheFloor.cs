dd089d4b5450a79c24e39247ba38b38f4d6c3ea6
cobypali <cobypali@gmail.com>
2019-04-13 17:30:36 -0700

    Ball and Racket

---
+++ Assets/DieIfYouHitTheFloor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieIfYouHitTheFloor : MonoBehaviour
+{
+    public GameObject floor;
+    public GameObject ball;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == floor)
+        {
+            Vector3 ballPosition = new Vector3(0.0f, 0.271f, -0.263f);
+            ball.transform.position = ballPosition;
+            ball.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
---
+++ Assets/addPhysics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addPhysics : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject racket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if(racket.position)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
