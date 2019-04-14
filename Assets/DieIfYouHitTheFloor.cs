348e9b41997dd5c1bacf780cfee6b1916cef6794
cobypali <cobypali@gmail.com>
2019-04-13 17:51:02 -0700

    Pre-Photon, ball respawn still doesn't fucking work

--- Assets/DieIfYouHitTheFloor.cs
+++ Assets/DieIfYouHitTheFloor.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class DieIfYouHitTheFloor : MonoBehaviour
 {
-    public GameObject floor;
-    public GameObject ball;
-
-    private void OnCollisionEnter(Collision collision)
+    private void update()
     {
-        if(collision.gameObject == floor)
+        print("hello");
+        if(transform.position.y < 0)
         {
+            print("Got to here");
             Vector3 ballPosition = new Vector3(0.0f, 0.271f, -0.263f);
-            ball.transform.position = ballPosition;
-            ball.GetComponent<Rigidbody>().isKinematic = true;
+            transform.position = ballPosition;
+            GetComponent<Rigidbody>().isKinematic = true;
         }
     }
 }
